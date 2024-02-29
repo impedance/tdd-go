a0e3b0485cde10f48cdbf9de4cf8a2048a9458e3
qvantt <2275399@gmail.com>
2024-02-29 13:03:33 +0300

    first hello

--- hello.go
+++ hello.go
@@ -1,11 +1,32 @@
 package main
-import "fmt"
+// import "fmt"
+
+const engilshHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 
-func Hello(name string) string {
-  return "Hello " + name
+func Hello(name string, language string) string {
+  if name == "" {
+    name = "World"
+  }
+
+  return greetingPrefix(language) + name
+
+}
+
+func greetingPrefix(language string) (prefix string) {
+
+  switch language {
+  case "French":
+    prefix = frenchHelloPrefix
+  case "Spanish":
+    prefix = spanishHelloPrefix
+  default:
+    prefix = engilshHelloPrefix
+  }
+  return 
 }
 
 func main() {
-  fmt.Println(Hello("world"))
 }
